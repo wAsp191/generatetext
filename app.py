d738f8e4457ea2f5ebea786e1ac5c2d0daf2d17d
wAsp191 <leonardo_diani@live.it>
2026-05-04 16:17:32 +0200

    Increase top padding of the page to 1.3rem

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.markdown("""
     <style>
         /* 1. Riduciamo il padding superiore della pagina */
         .block-container {
-            padding-top: 0.9rem !important;
+            padding-top: 1.3rem !important;
             padding-bottom: 0rem !important;
         }
 
