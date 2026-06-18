b7189268b300f4ba8266a7cb54b8f432055b2bd9
wAsp191 <leonardo_diani@live.it>
2026-06-18 14:40:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+import streamlit as st
 
+# 1. Configurazione della pagina
+st.set_page_config(page_title="Technical Generator", page_icon="🚀", layout="wide")
+
+# 2. Definiamo i puntatori ai file reali nelle cartelle
+# app.py farà solo da vigile urbano, reindirizzando subito ai file corretti
+pagina_generatore = st.Page("pages/0_Technical_Generator.py", title="Technical Generator", icon="🚀", default=True)
+pagina_analytics = st.Page("pages/1_Analytics.py", title="Analytics", icon="📊")
+
+# 3. Avvia la navigazione
+pg = st.navigation([pagina_generatore, pagina_analytics])
+pg.run()
